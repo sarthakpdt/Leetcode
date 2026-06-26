15cb2be9edb00b03358f70c9e002633b4fca5040
Sarthak Pandit <141760054+sarthakpdt@users.noreply.github.com>
2026-06-26 19:52:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (95.71%) - LeetHub

---
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.c
@@ -0,0 +1,17 @@
+char findKthBit(int n, int k) {
+    if (n==1){
+        return '0';
+    }
+    int len=(1<<n)-1;
+    int mid=len/2+1;
+    if (k==mid){
+        return '1';
+    }
+    else if(k<mid){
+        return findKthBit(n-1,k);
+    }
+    else{
+        char mb=findKthBit(n-1,len-k+1);
+        return (mb=='0')?'1':'0';
+    }
+}
