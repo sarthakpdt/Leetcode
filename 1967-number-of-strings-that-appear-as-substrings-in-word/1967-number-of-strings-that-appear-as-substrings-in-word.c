e6a12314ca72de43ebbf9bb8c85cf2de1d522094
Sarthak Pandit <141760054+sarthakpdt@users.noreply.github.com>
2026-06-29 11:34:45 +0530

    Time: 0 ms (100%), Space: 9.3 MB (32.35%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.c
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.c
@@ -1,23 +1,16 @@
 int numOfStrings(char** patterns, int patternsSize, char* word) {
-    int n=strlen(word);
-    int total=(n*(n+1))/2;
-    char substring[total][n+1];
-    int count1=0;
-    for (int i=0;i<n;i++){
-        for (int j=1;j<=n-i;j++){
-            strncpy(substring[count1],&word[i],j);
-            substring[count1][j]='\0';
-            count1++;
+
+    int c=0;
+
+    for(int i=0;i<patternsSize;i++)
+    {
+        if(strstr(word,patterns[i])!=NULL)
+        {
+            c++;
         }
+        
     }
-    int count=0;
-    for (int i=0;i<patternsSize;i++){
-        for (int j=0;j<total;j++){
-            if (strcmp(patterns[i],substring[j])==0){
-                count++;
-                break;
-            }
-        }
-    }
-    return count;
+
+    return c;
+    
 }
