02f6398f1635230410142b8ee8bf2ff410783011
Sarthak Pandit <141760054+sarthakpdt@users.noreply.github.com>
2026-07-25 22:49:40 +0530

    Time: 0 ms (100%), Space: 9.1 MB (43.08%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.c
@@ -0,0 +1,14 @@
+int maxProduct(int n) {
+    int first = 0, second = 0;
+    while (n > 0) {
+        int x = n % 10;
+        if (x > first) {
+            second = first;
+            first = x;
+        } else if (x > second) {
+            second = x;
+        }
+        n /= 10;
+    }
+    return first * second;
+}
