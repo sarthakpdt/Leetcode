04903754cd0f364a1a81b3e936ccc781382658ce
Sarthak Pandit <141760054+sarthakpdt@users.noreply.github.com>
2026-07-28 19:15:00 +0530

    Time: 4 ms (95.45%), Space: 97.9 MB (86.36%) - LeetHub

--- 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.c
+++ 3834-merge-adjacent-equal-elements/3834-merge-adjacent-equal-elements.c
@@ -2,16 +2,15 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 long long* mergeAdjacent(int* nums, int numsSize, int* returnSize) {
-    long long *stack=(long long *)malloc(numsSize*sizeof(long long));
-    int top=-1;
+    long long *stack=(long long *)malloc(sizeof(long long)*numsSize);
+    int top=0;
     for (int i=0;i<numsSize;i++){
-        stack[++top]=nums[i];
-        while (top>=1 && stack[top]==stack[top-1]){
-            long long sum=stack[top]+stack[top-1];
-            top-=2;
-            stack[++top]=sum;
+        stack[top++]=nums[i];
+        while (top>=2 && stack[top-1]==stack[top-2]){
+            stack[top-2]=stack[top-1]+stack[top-2];
+            top--;
         }
     }
-    *returnSize=top+1;
+    *returnSize=top;
     return stack;
 }
