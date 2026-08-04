edd15b715a7928d70030136311205ac24faedc22
Sarthak Pandit <141760054+sarthakpdt@users.noreply.github.com>
2026-08-04 20:35:46 +0530

    Time: 642 ms (32.19%), Space: 0B (100%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select Employees.employee_id from Employees left join Salaries on Employees.employee_id=Salaries.employee_id where Salaries.employee_id is null union select Salaries.employee_id from Salaries left join Employees on Salaries.employee_id=Employees.employee_id where Employees.employee_id is null order by employee_id;;
