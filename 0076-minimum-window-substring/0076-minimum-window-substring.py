13ef5665c9658a74bf5ff58bda07d38b25e0030d
Sarthak Pandit <141760054+sarthakpdt@users.noreply.github.com>
2026-08-13 11:19:31 +0530

    Time: 188 ms (21.75%), Space: 15.9 MB (23.49%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,28 +1,22 @@
 class Solution(object):
     def minWindow(self,s,t):
-        if len(t)>len(s):
-            return ""
-        freq={}
-        for ch in t:
-            freq[ch]=freq.get(ch,0)+1
-        left=0
+        f={}
+        for c in t:
+            f[c]=f.get(c,0)+1
+        l=0
         count=len(t)
-        start=0
-        minlen=float('inf')
-        for right in range(len(s)):
-            if s[right] in freq:
-                if freq[s[right]]>0:
+        ans=""
+        for r in range(len(s)):
+            if s[r] in f:
+                if f[s[r]]>0:
                     count-=1
-                freq[s[right]]-=1
+                f[s[r]]-=1
             while count==0:
-                if right-left+1<minlen:
-                    minlen=right-left+1
-                    start=left
-                if s[left] in freq:
-                    freq[s[left]]+=1
-                    if freq[s[left]]>0:
+                if not ans or r-l+1<len(ans):
+                    ans=s[l:r+1]
+                if s[l] in f:
+                    f[s[l]]+=1
+                    if f[s[l]]>0:
                         count+=1
-                left+=1
-        if minlen==float('inf'):
-            return ""
-        return s[start:start+minlen]
+                l+=1
+        return ans
