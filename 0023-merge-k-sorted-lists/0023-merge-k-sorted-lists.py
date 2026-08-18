ea7b9313c99354323ea77d347525aebf54b5d493
Sarthak Pandit <141760054+sarthakpdt@users.noreply.github.com>
2026-08-18 19:51:59 +0530

    Time: 13 ms (84.84%), Space: 18.2 MB (22.08%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,17 @@
+import heapq
+
+class Solution(object):
+    def mergeKLists(self,lists):
+        heap=[]
+        for i in range(len(lists)):
+            if lists[i]:
+                heapq.heappush(heap,(lists[i].val,i,lists[i]))
+        dummy=ListNode(0)
+        tail=dummy
+        while heap:
+            val,i,node=heapq.heappop(heap)
+            tail.next=node
+            tail=tail.next
+            if node.next:
+                heapq.heappush(heap,(node.next.val,i,node.next))
+        return dummy.next
