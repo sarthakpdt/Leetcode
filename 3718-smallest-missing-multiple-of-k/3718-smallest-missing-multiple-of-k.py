a14123c36daee566e3c4cfdc9b636e2b00059d9d
Sarthak Pandit <141760054+sarthakpdt@users.noreply.github.com>
2026-08-25 12:05:28 +0530

    Time: 0 ms (100%), Space: 12.4 MB (50.57%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -5,10 +5,7 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        res=[]
-        for i in range(1,102):
-            res.append(i*k)
-        for i in res:
-            if i not in nums:
-                return i
-        
+        i=1
+        while i*k in nums:
+            i+=1
+        return i*k
